29b79e32991044cdd2868fa4d2d89ac2cd1371fc
ilias37 <93328693+ilias37@users.noreply.github.com>
2025-03-09 23:25:57 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,5 @@ PASSWORD="your_password"
 for cmd in "apt update" "apt upgrade -y" "apt clean" "apt autoclean" "apt autoremove -y"; do
     echo -e "\033[1;33mExecuting \033[1;37m\"\033[1;33msudo $cmd\033[1;37m\"\033[0m"
     echo "$PASSWORD" | sudo -S $cmd
-    echo
-    echo
-    echo
+    echo -e "\n\n"
 done
