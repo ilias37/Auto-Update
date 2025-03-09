ec616258623a8e629b3399c6482668c4945fe09e
ilias37 <iliaspanag37@outlook.com>
2025-03-09 16:54:11 +0200

    Make the script simpler

--- update.sh
+++ update.sh
@@ -2,28 +2,10 @@
 
 PASSWORD="your_password"
 
-run() {
-   echo -e "\033[1;33mExecuting \033[1;37m\"\033[1;33msudo $1\033[1;37m\"\033[0m"
-   echo "$PASSWORD" | sudo -S $1
-}
-
-lines() {
-    for i in {1..3}; do
-        echo
-    done
-}
-
-run "apt update"
-lines
-
-run "apt upgrade -y"
-lines
-
-run "apt clean"
-lines
-
-run "apt autoclean"
-lines
-
-run "apt autoremove -y"
-lines
+for cmd in "apt update" "apt upgrade -y" "apt clean" "apt autoclean" "apt autoremove -y"; do
+    echo -e "\033[1;33mExecuting \033[1;37m\"\033[1;33msudo $cmd\033[1;37m\"\033[0m"
+    echo "$PASSWORD" | sudo -S $cmd
+    echo
+    echo
+    echo
+done
