3c609e0e82b320b005e67ccb9511d2148034d518
ilias37 <iliaspanag37@outlook.com>
2025-04-11 15:10:31 +0300

    Major security update

--- update.sh
+++ update.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-PASSWORD="your_password"
+sudo -k
 
-for cmd in "apt update" "apt upgrade -y" "apt clean" "apt autoclean" "apt autoremove -y"; do
+for cmd in \
+    "apt update" \
+    "apt full-upgrade -y" \
+    "apt clean" \
+    "apt autoclean" \
+    "apt autoremove -y"; do
     echo -e "\033[1;33mExecuting \033[1;37m\"\033[1;33msudo $cmd\033[1;37m\"\033[0m"
-    echo "$PASSWORD" | sudo -S $cmd
+    sudo $cmd
     echo -e "\n\n"
 done
